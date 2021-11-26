44069e121edcf6d019010eba79c91824ce5776d9
EFB <mynameisrone@gmail.com>
2021-11-26 18:19:15 +0530

    fix: cors

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("hello node");
